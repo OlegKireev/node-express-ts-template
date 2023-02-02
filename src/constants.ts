df73017e39b4f97b822176f802a9d4b4fc38bdea
Oleg Kireev <okireev@ubic.tech>
2023-02-02 17:18:10 +0400

    @/constants: create

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const ROOT = path.resolve(__dirname, '..');
+
+export const { PORT } = process.env;
+export const { DB_CONNECT } = process.env;
