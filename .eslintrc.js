d69098d06e352ab676f5f9d7b6e4f1d4b5ca32a5
Oleg Kireev <okireev@ubic.tech>
2023-02-02 17:17:32 +0400

    .eslintrc: change rules for typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,13 @@ module.exports = {
   extends: [
     'airbnb-base',
   ],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
@@ -15,5 +22,67 @@ module.exports = {
     '@typescript-eslint',
   ],
   rules: {
+    'import/no-extraneous-dependencies': 'off',
+    'no-console': 'off',
+    'import/prefer-default-export': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+    'import/exports-last': 'off',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'error',
+    '@typescript-eslint/type-annotation-spacing': 'error',
+    '@typescript-eslint/adjacent-overload-signatures': 'error',
+    '@typescript-eslint/array-type': [
+      'error',
+      {
+        default: 'array',
+        readonly: 'array',
+      },
+    ],
+    '@typescript-eslint/consistent-type-assertions': [
+      'warn',
+      {
+        assertionStyle: 'as',
+        objectLiteralTypeAssertions: 'allow-as-parameter',
+      },
+    ],
+    '@typescript-eslint/consistent-type-definitions': 'error',
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        fixStyle: 'inline-type-imports',
+      },
+    ],
+    '@typescript-eslint/member-delimiter-style': 'error',
+    '@typescript-eslint/member-ordering': [
+      'error',
+      {
+        default: {
+          optionalityOrder: 'optional-first',
+        },
+      },
+    ],
+    '@typescript-eslint/method-signature-style': 'warn',
+    '@typescript-eslint/no-dynamic-delete': 'warn',
+    '@typescript-eslint/no-non-null-asserted-nullish-coalescing': 'warn',
+    '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
   },
 };
