42ca411a4808afc7d0d3f4e5c654f906e4ae842f
Oleg Kireev <okireev@ubic.tech>
2023-02-02 17:20:13 +0400

    @/models/post: create

---
+++ src/models/post.model.ts
@@ -0,0 +1,18 @@
+import { Schema, model } from 'mongoose';
+
+const postSchema = new Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+}, { timestamps: true });
+
+export const Post = model('Post', postSchema);
