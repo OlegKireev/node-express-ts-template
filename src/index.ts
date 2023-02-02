0ab2a98271518644a97ef0a471e8add0c990ec62
Oleg Kireev <okireev@ubic.tech>
2023-02-02 17:22:25 +0400

    @/index: add routes, middlewares, db connection

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,30 @@
-import express, { Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+import { postsRouter } from './routes';
+import { DB_CONNECT, PORT } from './constants';
+import { colors } from './utils';
+import { errorHandler } from './middlewares';
 
-dotenv.config();
-
+/** App */
 const app = express();
-const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+app.listen(PORT, () => {
+  console.log(colors.success(`[server]: started at http://localhost:${PORT}`));
 });
 
-app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
+/** DB */
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(`${DB_CONNECT}`)
+  .then(() => console.log(colors.success('[db]: Connected!')))
+  .catch((err) => console.error(colors.error(`[db] Connecting error: ${err}`)));
+
+/** Routes */
+app.use(postsRouter);
+
+/** Error handle middleware */
+app.use(errorHandler);
