938ddf8550fbdbe75ecb043ede1716a01188859c
Oleg Kireev <okireev@ubic.tech>
2023-02-03 11:23:17 +0400

    @/controllers/user: store a hashed password in create

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,6 @@
 import { type Request, type Response } from 'express';
 import { User } from '../models';
-import { handleError } from '../utils';
+import { handleError, hashPassword } from '../utils';
 
 const getAll = (req: Request, res: Response) => {
   User.find()
@@ -25,16 +25,25 @@ const getById = (req: Request, res: Response) => {
     });
 };
 
-const create = (req: Request, res: Response) => {
-  const { username, password, role } = req.body;
-  const newUser = new User({ username, password, role });
-
-  newUser
-    .save()
-    .then((post) => res.status(200).json({ data: post }))
-    .catch((err) => {
-      handleError(res, err);
+const create = async (req: Request, res: Response) => {
+  try {
+    const { username, password, role } = req.body;
+    const hashedPassword = await hashPassword(password);
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+      role,
     });
+
+    const user = await newUser.save();
+    if (user) {
+      return res.status(200).json({ data: user });
+    }
+  } catch (err) {
+    if (err instanceof Error) {
+      handleError(res, err);
+    }
+  }
 };
 
 const updateById = (req: Request, res: Response) => {
