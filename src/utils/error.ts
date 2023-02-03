a2f41019bc47d7d8f605e3b70ac018b808f92c2f
Oleg Kireev <okireev@ubic.tech>
2023-02-03 11:26:37 +0400

    @/utils/error: change error type to unknown

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,7 +1,12 @@
 import { type Response } from 'express';
 
-export const handleError = (res: Response, error: Error) => {
-  res.status(500).send({
-    error: error.message,
+export const handleError = (res: Response, error: unknown) => {
+  if (error instanceof Error) {
+    return res.status(500).send({
+      error: error.message,
+    });
+  }
+  return res.status(500).send({
+    error: 'an unknown error',
   });
 };
