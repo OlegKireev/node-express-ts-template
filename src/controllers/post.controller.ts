73b4d8d813d0d97e10db416bfc81894bc0bf5c00
Oleg Kireev <okireev@ubic.tech>
2023-02-08 10:09:59 +0400

    @/controllers/post: rewrite promises to async/await syntaxis

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -2,71 +2,62 @@ import { type Request, type Response } from 'express';
 import { Post } from '../models';
 import { ExpressError, handleError } from '../utils';
 
-const getAll = (req: Request, res: Response) => {
-  Post.find()
-    .sort({ createdAt: -1 })
-    .then((posts) => {
-      res.status(200).json({ data: posts });
-    })
-    .catch((err) => {
-      handleError(res, err);
-    });
+const getAll = async (req: Request, res: Response) => {
+  try {
+    const posts = await Post
+      .find()
+      .sort({ createdAt: -1 });
+    return res.status(200).json({ data: posts });
+  } catch (err) {
+    handleError(res, err);
+  }
 };
 
-const getById = (req: Request, res: Response) => {
-  const { id } = req.params;
-
-  Post.findById(id)
-    .then((post) => {
-      res.status(200).json({ data: post });
-    })
-    .catch((err) => {
-      handleError(res, err);
-    });
+const getById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const post = await Post.findById(id);
+    return res.status(200).json({ data: post });
+  } catch (err) {
+    handleError(res, err);
+  }
 };
 
-const create = (req: Request, res: Response) => {
-  const { title, author, text } = req.body;
-  const newPost = new Post({ title, author, text });
-
-  newPost
-    .save()
-    .then((post) => res.status(200).json({ data: post }))
-    .catch((err) => {
-      handleError(res, err);
-    });
+const create = async (req: Request, res: Response) => {
+  try {
+    const { title, author, text } = req.body;
+    const newPost = new Post({ title, author, text });
+    const post = await newPost.save();
+    return res.status(200).json({ data: post });
+  } catch (err) {
+    handleError(res, err);
+  }
 };
 
-const updateById = (req: Request, res: Response) => {
-  const { title, author, text } = req.body;
-  const { id } = req.params;
-
-  Post
-    .findByIdAndUpdate(id, { title, author, text }, { new: true })
-    .then((post) => {
-      res.status(200).json({ data: post });
-    })
-    .catch((err) => {
-      handleError(res, err);
-    });
+const updateById = async (req: Request, res: Response) => {
+  try {
+    const { title, author, text } = req.body;
+    const { id } = req.params;
+    const post = await Post.findByIdAndUpdate(id, { title, author, text }, { new: true });
+    return res.status(200).json({ data: post });
+  } catch (err) {
+    handleError(res, err);
+  }
 };
 
-const deleteById = (req: Request, res: Response) => {
-  const { id } = req.params;
-
-  Post.findByIdAndDelete(id)
-    .then((post) => {
-      if (post) {
-        res.status(200).json(post._id);
-      } else {
-        throw new ExpressError({
-          message: `There is no post with id ${id}`,
-        });
-      }
-    })
-    .catch((err) => {
-      handleError(res, err);
+const deleteById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const post = await Post.findByIdAndDelete(id);
+    if (post) {
+      return res.status(200).json(post._id);
+    }
+    throw new ExpressError({
+      message: `There is no post with id ${id}`,
     });
+  } catch (err) {
+    handleError(res, err);
+  }
 };
 
 export default {
